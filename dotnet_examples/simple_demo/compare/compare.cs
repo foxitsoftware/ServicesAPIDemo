010308d666311fd8f25fd9bd97e20df3c74bdaad
hong_lin <hong_lin@foxitsoftware.com>
2024-06-17 17:31:20 +0800

    complete sn for compare.

--- dotnet_examples/simple_demo/compare/compare.cs
+++ dotnet_examples/simple_demo/compare/compare.cs
@@ -12,72 +12,111 @@
 
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using RestSharp;
 
 namespace CompareCS
 {
-  public class RestException:Exception
-  {
-    public RestException(RestResponse r, string message, Exception innerException)
-      :base(message, innerException)
+    public class RestException : Exception
     {
-      response = r;
-    }
-    public RestResponse response { get; private set; }
-  }
-
-  class Compare
-  {
-    private string client_id = "";
-    private string secret_id = "";
-    // The signature of parameters, clientId and secret Id. (we ignore this parameter  on trial version，input any string is ok)
-    private string sn = "testsn";
-    // TODO: replace with your own input doc path and output file path
-    private const string input_file_path1 = "../input_files/test_base.pdf";
-    private const string input_file_path2 = "../input_files/test_compared.pdf";
-    private const string output_file_path = "../output_files/compare/CompareResultFiles.json";
-
-    //TODO: replace with server base url
-    private IRestClient client = new RestClient(new RestClientOptions() { MaxTimeout = 60 * 1000, BaseUrl = new System.Uri("https://servicesapi.foxitsoftware.cn/api") });
-
-    // Read clientId and secretId form the json file.
-    private void GetCredentialsParams(string credentials_path)
-    {
-      string content = File.ReadAllText(credentials_path);
-      dynamic json = Newtonsoft.Json.Linq.JToken.Parse(content) as dynamic;
-      client_id = json.client_credentials.client_id;
-      secret_id = json.client_credentials.secret_id;
+        public RestException(RestResponse r, string message, Exception innerException)
+          : base(message, innerException)
+        {
+            response = r;
+        }
+        public RestResponse response { get; private set; }
     }
 
-    private string ComparePDFTask(string input_file_base, string input_file_compare)
+    class Compare
     {
-      var request = new RestRequest("document/compare", Method.Post);
-      request
-        .AddHeader("Accept", "application/json")
-        .AddQueryParameter("sn", sn)
-        .AddQueryParameter("clientId", client_id)        
-        .AddFile("inputBaseDocument", input_file_base, "multipart/form-data")
-        .AddFile("inputCompareDocument", input_file_compare, "multipart/form-data")
-        .AddParameter("resultType", "json")
-        .AddParameter("compareType", "all");
-      // Upload a file and create a new workflow task.
-      var response = client.ExecuteAsync(request);
-      if (response.Result.IsSuccessful &&
-            response.Result.ResponseStatus == ResponseStatus.Completed)
-      {
-        dynamic json = Newtonsoft.Json.Linq.JToken.Parse(response.Result.Content) as dynamic;
-        if (json.code == 0) return json.data.taskInfo.taskid;
-      }
+        private string client_id = "";
+        private string secret_id = "";
+        // The signature of parameters, clientId and secret Id. (we ignore this parameter  on trial version，input any string is ok)
+        private string sn = "testsn";
+        // TODO: replace with your own input doc path and output file path
+        private const string input_file_path1 = "../input_files/test_base.pdf";
+        private const string input_file_path2 = "../input_files/test_compared.pdf";
+        private const string output_file_path = "../output_files/compare/CompareResultFiles.json";
 
-      string message = string.Format("http response error: {0} : {1}",
-             response.Result.ErrorMessage, response.Result.Content);
-      throw new RestException(response.Result, message, response.Result.ErrorException);
-    }
+        //TODO: replace with server base url
+        private IRestClient client = new RestClient(new RestClientOptions() { MaxTimeout = 60 * 1000, BaseUrl = new System.Uri("https://serviceapi-devcn.connectedpdf.com/api") });
 
-    private string GetTaskInfo(string task_id)
-    {
+        static string GenerateMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputbytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashbytes = md5.ComputeHash(inputbytes);
+                return BitConverter.ToString(hashbytes).Replace("-", "").ToLower();
+            }
+        }
+
+        // Read clientId and secretId form the json file.
+        private void GetCredentialsParams(string credentials_path)
+        {
+            string content = File.ReadAllText(credentials_path);
+            dynamic json = Newtonsoft.Json.Linq.JToken.Parse(content) as dynamic;
+            client_id = json.client_credentials.client_id;
+            secret_id = json.client_credentials.secret_id;
+        }
+
+        private string ComparePDFTask(string input_file_base, string input_file_compare)
+        {
+            string result_type = "json";
+            string compare_type = "all";
+            var query_params = new Dictionary<string, string>
+            {
+                { "clientId", client_id },
+                { "resultType", result_type },
+                { "compareType", compare_type}
+            };
+            var sorted_params = query_params.OrderBy(kv => kv.Key).ToDictionary(kv => kv.Key, kv => kv.Value);
+            var query_string = string.Join("&", sorted_params.Select(kv => $"{kv.Key}={WebUtility.UrlEncode(kv.Value)}"));
+            query_string += "&sk=" + secret_id;
+            sn = GenerateMD5(query_string);
+
+            var request = new RestRequest("document/compare", Method.Post);
+            request
+              .AddHeader("Accept", "application/json")
+              .AddQueryParameter("sn", sn)
+              .AddQueryParameter("clientId", client_id)
+              .AddFile("inputBaseDocument", input_file_base, "multipart/form-data")
+              .AddFile("inputCompareDocument", input_file_compare, "multipart/form-data")
+              .AddParameter("resultType", result_type)
+              .AddParameter("compareType", compare_type);
+
+            // Upload a file and create a new workflow task.
+            var response = client.ExecuteAsync(request);
+            if (response.Result.IsSuccessful &&
+                  response.Result.ResponseStatus == ResponseStatus.Completed)
+            {
+                dynamic json = Newtonsoft.Json.Linq.JToken.Parse(response.Result.Content) as dynamic;
+                if (json.code == 0) return json.data.taskInfo.taskId;
+            }
+
+            string message = string.Format("http response error: {0} : {1}",
+                   response.Result.ErrorMessage, response.Result.Content);
+            throw new RestException(response.Result, message, response.Result.ErrorException);
+        }
+
+        private string GetTaskInfo(string task_id)
+        {
+            var query_params = new Dictionary<string, string>
+            {
+                { "clientId", client_id },
+                { "taskId", task_id }
+            };
+            var sorted_params = query_params.OrderBy(kv => kv.Key).ToDictionary(kv => kv.Key, kv => kv.Value);
+            var query_string = string.Join("&", sorted_params.Select(kv => $"{kv.Key}={WebUtility.UrlEncode(kv.Value)}"));
+            query_string += "&sk=" + secret_id;
+            sn = GenerateMD5(query_string);
+
             RestRequest request = new RestRequest("task", Method.Get);
             request
               .AddHeader("Accept", "application/json")
@@ -102,38 +141,49 @@ namespace CompareCS
             throw new RestException(response.Result, message, response.Result.ErrorException);
         }
 
-    private string PollForDocId(string task_id, int interval_in_miliseconds = 2000)
-    {
-      do
-      {
-        try
-        {          
-          string task_info = GetTaskInfo(task_id);
-          dynamic json = Newtonsoft.Json.Linq.JToken.Parse(task_info) as dynamic;
-          int percentage = json.percentage;
-          if (percentage == 100)
-          {
-            Console.WriteLine("Task completed.");
-            return json.docid;
-          } 
-        }
-        catch (RestException e)
+        private string PollForDocId(string task_id, int interval_in_miliseconds = 2000)
         {
-          dynamic tmp = Newtonsoft.Json.Linq.JToken.Parse(e.response.Content) as dynamic;
-          string detail = tmp.data.detail;
-          // when task is running, the task api will return error
-          // if task is running, try to get taskInfo later 
-          if (detail.IndexOf("The task is running") > -1)
-            Console.WriteLine("Task is running, retry in {0} miliseconds", interval_in_miliseconds);
-          else
-            throw e;                       
+            do
+            {
+                try
+                {
+                    string task_info = GetTaskInfo(task_id);
+                    dynamic json = Newtonsoft.Json.Linq.JToken.Parse(task_info) as dynamic;
+                    int percentage = json.percentage;
+                    if (percentage == 100)
+                    {
+                        Console.WriteLine("Task completed.");
+                        return json.docId;
+                    }
+                }
+                catch (RestException e)
+                {
+                    dynamic tmp = Newtonsoft.Json.Linq.JToken.Parse(e.response.Content) as dynamic;
+                    string detail = tmp.data.detail;
+                    // when task is running, the task api will return error
+                    // if task is running, try to get taskInfo later 
+                    if (detail.IndexOf("The task is running") > -1)
+                        Console.WriteLine("Task is running, retry in {0} miliseconds", interval_in_miliseconds);
+                    else
+                        throw e;
+                }
+                Task.Delay(interval_in_miliseconds).Wait();
+            } while (true);
         }
-        Task.Delay(interval_in_miliseconds).Wait();
-      } while (true);
-    }
 
-    private void DownLoadFileByDocId(string doc_id, string output_file_path)
-    {
+        private void DownLoadFileByDocId(string doc_id, string output_file_path)
+        {
+            var query_params = new Dictionary<string, string>
+            {
+               { "clientId", client_id },
+               { "docId", doc_id },
+               { "fileName", Path.GetFileName(output_file_path)}
+            };
+            var sorted_params = query_params.OrderBy(kv => kv.Key).ToDictionary(kv => kv.Key, kv => kv.Value);
+            var query_string = string.Join("&", sorted_params.Select(kv => $"{kv.Key}={WebUtility.UrlEncode(kv.Value)}"));
+            query_string += "&sk=" + secret_id;
+            sn = GenerateMD5(query_string);
+
             var request = new RestRequest("download", Method.Get);
             request
               .AddQueryParameter("sn", sn)
@@ -157,34 +207,34 @@ namespace CompareCS
             }
         }
 
-    public static void Start()
-    {
-      string output_path = Path.GetDirectoryName(output_file_path);
-      if (Directory.Exists(output_path) == false)
-        Directory.CreateDirectory(output_path);
+        public static void Start()
+        {
+            string output_path = Path.GetDirectoryName(output_file_path);
+            if (Directory.Exists(output_path) == false)
+                Directory.CreateDirectory(output_path);
 
-      try
-      {
-        Compare compare = new Compare();
-        compare.GetCredentialsParams(Directory.GetCurrentDirectory() + "/foxit_cloud_api_credentials.json");
-        string task_id = compare.ComparePDFTask(input_file_path1, input_file_path2);
-        string doc_id = compare.PollForDocId(task_id);
+            try
+            {
+                Compare compare = new Compare();
+                compare.GetCredentialsParams(Directory.GetCurrentDirectory() + "/foxit_cloud_api_credentials.json");
+                string task_id = compare.ComparePDFTask(input_file_path1, input_file_path2);
+                string doc_id = compare.PollForDocId(task_id);
                 compare.DownLoadFileByDocId(doc_id, output_file_path);
-        Console.WriteLine("Compare PDF files successfully!");
-      }
-      catch(RestException e)
-      {
-        Console.WriteLine(e.Message);
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine(e.Message);
-      }
-    }
+                Console.WriteLine("Compare PDF files successfully!");
+            }
+            catch (RestException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
 
-    static void Main(string[] args)
-    {
-      Compare.Start();
+        static void Main(string[] args)
+        {
+            Compare.Start();
+        }
     }
-  }
 }
